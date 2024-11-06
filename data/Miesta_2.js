d969a72e28bb9a6673b791df03c761539834a91d
EricockTerminator <krhliak02@gmail.com>
2024-11-06 19:09:46 +0100

    Add files via upload

---
+++ data/Miesta_2.js
@@ -0,0 +1,27 @@
+var json_Miesta_2 = {
+"type": "FeatureCollection",
+"name": "Miesta_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "route_fid": "0", "route_poin": "0", "ele": 758.81799, "miesto": "Bishkek" }, "geometry": { "type": "Point", "coordinates": [ 74.606693, 42.877789 ] } },
+{ "type": "Feature", "properties": { "route_fid": "0", "route_poin": "1", "ele": 986.0, "miesto": "Toktogul" }, "geometry": { "type": "Point", "coordinates": [ 72.945105, 41.87347 ] } },
+{ "type": "Feature", "properties": { "route_fid": "0", "route_poin": "2", "ele": 1489.75452, "miesto": "Arslanbob" }, "geometry": { "type": "Point", "coordinates": [ 72.927713, 41.337624 ] } },
+{ "type": "Feature", "properties": { "route_fid": "0", "route_poin": "3", "ele": 1060.03418, "miesto": "O" }, "geometry": { "type": "Point", "coordinates": [ 72.79101, 40.530361 ] } },
+{ "type": "Feature", "properties": { "route_fid": "0", "route_poin": "4", "ele": 2064.81934, "miesto": "Sary-Korlon" }, "geometry": { "type": "Point", "coordinates": [ 73.494005, 40.019895 ] } },
+{ "type": "Feature", "properties": { "route_fid": "0", "route_poin": "5", "ele": 3162.38257, "miesto": "Sary-Tash" }, "geometry": { "type": "Point", "coordinates": [ 73.248879, 39.728227 ] } },
+{ "type": "Feature", "properties": { "route_fid": "0", "route_poin": "7", "ele": 3942.19946, "miesto": "Severný Aral (Tajikistan)" }, "geometry": { "type": "Point", "coordinates": [ 73.442298, 39.082332 ] } },
+{ "type": "Feature", "properties": { "route_fid": "0", "route_poin": "8", "ele": 1294.00537, "miesto": "Kašgar" }, "geometry": { "type": "Point", "coordinates": [ 75.990013, 39.466598 ] } },
+{ "type": "Feature", "properties": { "route_fid": "0", "route_poin": "9", "ele": 1107.39661, "miesto": "Aksu" }, "geometry": { "type": "Point", "coordinates": [ 80.258187, 41.16867 ] } },
+{ "type": "Feature", "properties": { "route_fid": "0", "route_poin": "10", "ele": 1065.47681, "miesto": "Kuqa" }, "geometry": { "type": "Point", "coordinates": [ 82.954434, 41.713496 ] } },
+{ "type": "Feature", "properties": { "route_fid": "0", "route_poin": "11", "ele": 934.0799, "miesto": "Korla" }, "geometry": { "type": "Point", "coordinates": [ 86.173541, 41.723874 ] } },
+{ "type": "Feature", "properties": { "route_fid": "0", "route_poin": "12", "ele": 30.0, "miesto": "Turpan" }, "geometry": { "type": "Point", "coordinates": [ 89.179589, 42.942503 ] } },
+{ "type": "Feature", "properties": { "route_fid": "0", "route_poin": "13", "ele": 1120.71643, "miesto": "Urumqi" }, "geometry": { "type": "Point", "coordinates": [ 87.489054, 43.727113 ] } },
+{ "type": "Feature", "properties": { "route_fid": "0", "route_poin": "14", "ele": 1770.79688, "miesto": "Baluntai" }, "geometry": { "type": "Point", "coordinates": [ 86.315388, 42.758473 ] } },
+{ "type": "Feature", "properties": { "route_fid": "0", "route_poin": "15", "ele": 2459.0083, "miesto": "Bayanulag" }, "geometry": { "type": "Point", "coordinates": [ 84.148585, 43.029067 ] } },
+{ "type": "Feature", "properties": { "route_fid": "0", "route_poin": "16", "ele": 790.27387, "miesto": "Ili" }, "geometry": { "type": "Point", "coordinates": [ 81.524523, 43.97581 ] } },
+{ "type": "Feature", "properties": { "route_fid": "0", "route_poin": "17", "ele": 633.50409, "miesto": "Žarkent" }, "geometry": { "type": "Point", "coordinates": [ 80.002739, 44.165665 ] } },
+{ "type": "Feature", "properties": { "route_fid": "0", "route_poin": "18", "ele": 1243.44324, "miesto": "Altyn Emel" }, "geometry": { "type": "Point", "coordinates": [ 78.445889, 44.335531 ] } },
+{ "type": "Feature", "properties": { "route_fid": "0", "route_poin": "19", "ele": 1007.76581, "miesto": "Charynský kaňon" }, "geometry": { "type": "Point", "coordinates": [ 79.076221, 43.356971 ] } },
+{ "type": "Feature", "properties": { "route_fid": "0", "route_poin": "20", "ele": 896.99695, "miesto": "Almaty" }, "geometry": { "type": "Point", "coordinates": [ 76.945727, 43.236392 ] } }
+]
+}
